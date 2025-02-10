4dc09ea493510320b4e8e6565e3d9a41dadd0de4
STTThore <t.lenz+github@softtailor.de>
2025-02-10 16:51:46 +0100

    Update Uploadandgo.ps1

--- Uploadandgo.ps1
+++ Uploadandgo.ps1
@@ -1,2 +1,2 @@
 Install-Script -Name Get-WindowsAutopilotInfo -Force
-Get-WindowsAutopilotInfo -Online -Grouptag "Köln" -TenantID "59bbf94b-a120-416b-9ffd-93fd07ca7401"
+Get-WindowsAutopilotInfo -Online -Grouptag "Koeln" -TenantID "59bbf94b-a120-416b-9ffd-93fd07ca7401"
